26a6c6db8a137cb6e1327883375b37ff8e6ee364
TijsDeBelie <Tijsdebelie1997@gmail.com>
2018-12-28 13:52:03 +0100

    ?

---
+++ Calender/Calender/Classes/Herhaling.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calender
+{
+    public class Herhaling
+    {
+        public Herhaling()
+        {
+            Herhalingen = new List<string>();
+            Herhalingen.Add("Geen");
+            Herhalingen.Add("Dagelijks");
+            Herhalingen.Add("Wekelijks");
+        }
+
+        public IList<string> Herhalingen { get; set; }
+    }
+}
